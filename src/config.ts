e0ca73277733b9f79e6db867696d57e72dff6c72
Sir-Antonic <8020285+Sir-Antonic@users.noreply.github.com>
2024-08-23 15:19:50 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export const SITE: Site = {
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
-  postPerPage: 3,
+  postPerPage: 5,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
